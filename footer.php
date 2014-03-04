bf10e5509972ea100cf6946f5668960db19d36a0
Andrew Lohman <ajlohman@gmail.com>
2014-03-03 22:50:06 -0600

    first commit - added all theme files

---
+++ footer.php
@@ -0,0 +1,24 @@
+			
+
+
+
+			<footer class="container-fluid" role="contentinfo" style="background:pink; padding-top:100px; padding-bottom:100px;">
+				<div class="row">
+					<div class="col-sm-12">
+						<!-- <nav role="navigation">
+								<?php bones_footer_links(); ?>
+						</nav> -->
+
+						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					</div>
+				</div>
+			</footer>
+
+		</div>
+
+		<?php // all js scripts are loaded in library/bones.php ?>
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html>
